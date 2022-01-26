5af2f064d85f939b7cadad0224909818668d1d79
jiey10 <88052268+jiey10@users.noreply.github.com>
2022-01-26 02:06:24 -0600

    Add files via upload

---
+++ Peliculas.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Peliculas.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Peliculas.Domain/Entities/Pelicula.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Peliculas.Domain.Entities
+{
+    public partial class Pelicula
+    {
+        public int IdPelicula { get; set; }
+        public string NombrePelicula { get; set; }
+        public string DirectorPelicula { get; set; }
+        public string GeneroPelicula { get; set; }
+        public int? CalfPelicula { get; set; }
+        public int? PuntPelicula { get; set; }
+        public string AnoPelicula { get; set; }
+    }
+}
